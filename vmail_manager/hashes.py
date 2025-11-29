238e378e92d250b66dc2b750ee362bb1b1a1d7ae
Marinus Enzinger <marinus@enzingerm.de>
2025-11-29 14:56:37 +0100

    Fix crypt library

--- vmail_manager/hashes.py
+++ vmail_manager/hashes.py
@@ -1,4 +1,4 @@
-import crypt
+import crypt_r
 
 SHA512_PRE = "{SHA512-CRYPT}"
 
@@ -6,10 +6,10 @@ def check_pw(password, crypted):
     if crypted.startswith(f"{SHA512_PRE}$6$"):
         without_scheme = crypted.removeprefix(f"{SHA512_PRE}$6$")
         salt, hash = without_scheme.split("$")
-        computed = crypt.crypt(password, f"$6${salt}")
+        computed = crypt_r.crypt(password, f"$6${salt}")
         return SHA512_PRE + computed == crypted
     raise Exception("Hash algorithm not supported!")
 
 def create_hash(password):
-    salt = crypt.mksalt(crypt.METHOD_SHA512)
-    return SHA512_PRE + crypt.crypt(password, salt)
+    salt = crypt_r.mksalt(crypt.METHOD_SHA512)
+    return SHA512_PRE + crypt_r.crypt(password, salt)
